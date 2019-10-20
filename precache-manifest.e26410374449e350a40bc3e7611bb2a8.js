4fecc0c29ea656b21a9b6a5538795b8a57ae0399
fwilbor <franklin.cfg@gmail.com>
2019-10-19 23:49:27 -0400

    Updates

---
+++ precache-manifest.e26410374449e350a40bc3e7611bb2a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfc1944d6c4d96337cccb7e5745530ea",
+    "url": "/Google-Mern-BookApp/index.html"
+  },
+  {
+    "revision": "09fd9d4b59ee0fd4c632",
+    "url": "/Google-Mern-BookApp/static/css/main.07e6d928.chunk.css"
+  },
+  {
+    "revision": "fc663d50b316b80374d6",
+    "url": "/Google-Mern-BookApp/static/js/2.7a883396.chunk.js"
+  },
+  {
+    "revision": "09fd9d4b59ee0fd4c632",
+    "url": "/Google-Mern-BookApp/static/js/main.0e7f386d.chunk.js"
+  },
+  {
+    "revision": "71bdc5aca5d9d24d3376",
+    "url": "/Google-Mern-BookApp/static/js/runtime-main.fd612d44.js"
+  }
+]);
