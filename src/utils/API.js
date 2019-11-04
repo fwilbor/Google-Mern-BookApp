6dc56a3a6142024e9870963bae19e120d3d54df4
fwilbor <franklin.cfg@gmail.com>
2019-11-04 10:00:41 -0500

    update yarn lock

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,13 +3,38 @@ import axios from "axios";
 // Export an object containing methods we'll use for accessing the Dog.Ceo API
 
 export default {
-  getRandonDog: function () {
+  getRandomdog: function () {
     return axios.get("https://dog.ceo/api/breeds/image/random");
   },
   getBooksofName: function (book) {
-    return axios.get("https://dog.ceo/api/breed/" + book + "/images");
+    return axios.get("https://www.googleapis.com/books/v1/volumes?q=search+terms");
   },
   getBooksList: function () {
     return axios.get("https://dog.ceo/api/breeds/list");
   }
 };
+
+
+
+// import http from "../services/httpService";
+
+// export default {
+//   // Gets books from the Google API
+//   getBooks: function(q) {
+//     return http.get("/api/google", {
+//       params: { q: "title:" + q }
+//     });
+//   },
+//   // Gets all saved books
+//   getSavedBooks: function() {
+//     return http.get("/api//books");
+//   },
+//   // Deletes the saved book with the given id
+//   deleteBook: function(id) {
+//     return http.delete("/api//books/" + id);
+//   },
+//   // Saves an book to the database
+//   saveBook: function(bookData) {
+//     return http.post("/api//books", bookData);
+//   }
+// };
